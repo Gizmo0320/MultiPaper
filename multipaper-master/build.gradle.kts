06a96da77bcdf1e8cc8a6d3d3074d924a615e78d
Gizmo0320 <gizmo0320@unleveledgaming.com>
2025-07-30 20:06:24 +0000

    Update Master Deps

--- multipaper-master/build.gradle.kts
+++ multipaper-master/build.gradle.kts
@@ -18,11 +18,11 @@ repositories {
 dependencies {
     implementation(project(":multipaper-mastermessagingprotocol"))
     implementation("org.jetbrains:annotations:22.0.0")
-    implementation("org.json:json:20250517")
+    implementation("org.json:json:20211205")
     implementation("org.yaml:snakeyaml:1.33")
-    implementation("io.netty:netty-all:4.1.118.Final")
+    implementation("io.netty:netty-all:4.1.87.Final")
     implementation("se.llbit:jo-nbt:1.3.0")
-    compileOnly("net.md-5:bungeecord-api:1.21-R0.3")
+    compileOnly("net.md-5:bungeecord-api:1.16-R0.4")
     compileOnly("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
     annotationProcessor("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
 }
